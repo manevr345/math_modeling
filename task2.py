b2cf3f2463056ba8efc4934a920c14459dd6660d
manevr345 <manevrik39@yandex.ru>
2026-03-05 14:51:20 +0000

    gfh

--- task2.py
+++ task2.py
@@ -7,29 +7,36 @@ frames = 200
 m = 0.8 
 k = 500
 T = 2 * np.pi * np.sqrt(m/k)
-t = np.linspace(-T, T, frames)
+t = np.linspace(0, 2*T, frames)
 def move_func(z, t):
-    y, vy, x0 = z 
+    
+    y, vy, x, vx = z 
     dy_dt = vy
-    dvy_dt = -g - k*vy*t / m
-    dx_dt = 0
-    return dy_dt, dvy_dt, dx_dt
+
+    dvy_dt = g - k*vy*t / m
+
+    dx_dt = x
+    dvx_dt = vx
+    
+    return dy_dt, dvy_dt, x, dvx_dt
 g = 9.8
 x0 = 0
 y0= 0
+x0 = 0
+vx0 = 0
 vy0 = 5
-z0 = y0, vy0, x0
+z0 = y0, vy0, x0, vx0
 solve = odeint(move_func, z0, t)
+
 fig, ax = plt.subplots()
 ball, = plt.plot([], [], 'o', color = 'red')
 ball_line, = plt.plot([], [], '-', color='red')
 
 def animate(i):
     ball.set_data([solve[i][0]], [solve[i][1]])
-    ball_line.set_data(solve[:i, 0], solve[:i, 2])
-ani = FuncAnimation(fig, animate, frames=frames, interval=30)
+ani = FuncAnimation(fig, animate, frames=frames, interval=100)
 
 edge = 15
-ax.set_ylim(-edge,  edge)
-ax.set_xlim(-edge, edge)
+ax.set_ylim(-3*edge,  3*edge)
+ax.set_xlim(-3*edge, 3*edge)
 ani.save('ani2.gif', writer='pillow')
